78305ebeab57394b48c26cf73d9af2c351afaeda
tolentinomarymae <tolentinomarymae1503@gmail.com>
2023-10-08 21:43:55 +0800

    vuefile

---
+++ dist/js/about.313cc5f3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunklab1vue"]=self["webpackChunklab1vue"]||[]).push([[443],{833:function(n,u,a){a.r(u),a.d(u,{default:function(){return b}});var e=a(3396);const t={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,e.wg)(),(0,e.iD)("div",t,s)}var i=a(89);const l={},o=(0,i.Z)(l,[["render",c]]);var b=o}}]);
+//# sourceMappingURL=about.313cc5f3.js.map
