7ccc542b5120135abdc132ed62ee78e3de0fb2e5
tolentinomarymae <tolentinomarymae1503@gmail.com>
2023-10-09 20:31:26 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Test from '../views/Test.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Test
   },
   {
     path: '/about',
---
+++ src/views/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class ="main-wrapper">
+	<Header/>
+	<Hero/>
+	<Intro/>
+	<Featured/>
+	<Product/>
+  </div>
+</template>
+
+<script>
+	import Header from '@/components/the-plaza/Header.vue';
+	import Hero from '@/components/the-plaza/Hero.vue';
+	import Intro from '@/components/the-plaza/Intro.vue';
+	import Featured from '@/components/the-plaza/Featured.vue';
+	import Product from '@/components/the-plaza/Product.vue';
+export default {
+  name: 'Test',
+  components:{
+	Header,
+	Hero,
+	Intro,
+	Featured,
+	Product,
+  }
+};
+</script>
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i');
+  @import '@/assets/the-plaza2/css/bootstrap.min.css'; 
+  @import '@/assets/the-plaza2/css/font-awesome.min.css';
+  @import '@/assets/the-plaza2/css/owl.carousel.css';
+  @import '@/assets/the-plaza2/css/style.css';
+  @import '@/assets/the-plaza2/css/animate.css';
+</style>
