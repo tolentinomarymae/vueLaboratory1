e4da6e7951fd44665aae61f86e6506ff5fb7d278
montellanocharmel <charmelmontellano29@gmail.com>
2023-10-12 21:21:14 +0800

    forward slash temp

---
+++ src/components/Design.vue
@@ -0,0 +1,36 @@
+<style>
+/* Import your CSS files here */
+  @import '@/assets/assets/css/style.css';
+  @import '@/assets/assets/css/bootstrap.css';
+  @import '@/assets/assets/css/responsive.css';
+
+</style>
+
+
+<script>
+  export default {
+    name: "Style",
+  mounted() {
+      this.loadScripts();
+  },
+  methods: {
+      loadScripts() {
+        const scriptUrls = [
+        '/public/js/bootstrap.min.js',
+        '/public/js/custom.js',
+        '/public/js/jquery-3.4.1.min.js',
+        ];
+
+        const head = document.getElementsByTagName('head')[0];
+
+        scriptUrls.forEach((scriptUrl) => {
+          const script = document.createElement('script');
+          script.src = scriptUrl;
+          script.async = true;
+          head.appendChild(script);
+        });
+      },
+  
+    },
+  };
+</script>
---
+++ src/components/Products.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+          <!-- item section -->
+
+  <div class="item_section layout_padding2">
+    <div class="container">
+      <div class="item_container">
+        <div class="box">
+          <div class="price">
+            <h6>
+              Best PRICE
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/i-1.png" alt="">
+          </div>
+          <div class="name">
+            <h5>
+              Bracelet
+            </h5>
+          </div>
+        </div>
+        <div class="box">
+          <div class="price">
+            <h6>
+              Best PRICE
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/i-2.png" alt="">
+          </div>
+          <div class="name">
+            <h5>
+              Ring
+            </h5>
+          </div>
+        </div>
+        <div class="box">
+          <div class="price">
+            <h6>
+              Best PRICE
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/i-3.png" alt="">
+          </div>
+          <div class="name">
+            <h5>
+              Earings
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- end item section -->
+
+
+    <!-- price section -->
+
+  <section class="price_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Our Jewellery Price
+        </h2>
+      </div>
+      <div class="price_container">
+        <div class="box">
+          <div class="name">
+            <h6>
+              Diamond Ring
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/p-1.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              $<span>1000.00</span>
+            </h5>
+            <a href="">
+              Buy Now
+            </a>
+          </div>
+        </div>
+        <div class="box">
+          <div class="name">
+            <h6>
+              Diamond Ring
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/i-2.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              $<span>1000.00</span>
+            </h5>
+            <a href="">
+              Buy Now
+            </a>
+          </div>
+        </div>
+        <div class="box">
+          <div class="name">
+            <h6>
+              Diamond Ring
+            </h6>
+          </div>
+          <div class="img-box">
+            <img src="./images/i-3.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              $<span>1000.00</span>
+            </h5>
+            <a href="">
+              Buy Now
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center">
+        <a href="" class="price_btn">
+          See More
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end price section -->
+
+    </div>
+</template>
